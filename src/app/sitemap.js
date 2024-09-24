01dd10deb5e8e9048a44f8f35157bb48f497445b
fastestsunil <sunil141292@gmail.com>
2024-09-24 13:22:04 +0530

    sitemap, robots txt, menu updated

---
+++ src/app/sitemap.js
@@ -0,0 +1,34 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://www.thetaprooms.com/',
+      lastModified: new Date(),
+      // changeFrequency: 'yearly',
+      // priority: 1,
+    },
+    {
+      url: 'https://www.thetaprooms.com/about',
+      lastModified: new Date(),
+      // changeFrequency: 'monthly',
+      // priority: 0.8,
+    },
+    {
+      url: 'https://www.thetaprooms.com/menu',
+      lastModified: new Date(),
+      // changeFrequency: 'weekly',
+      // priority: 0.5,
+    },
+    {
+      url: 'https://www.thetaprooms.com/reservation',
+      lastModified: new Date(),
+      // changeFrequency: 'weekly',
+      // priority: 0.5,
+    },
+    {
+      url: 'https://www.thetaprooms.com/contact',
+      lastModified: new Date(),
+      // changeFrequency: 'weekly',
+      // priority: 0.5,
+    },
+  ];
+}
